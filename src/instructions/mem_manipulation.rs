bc06173380dd127e38be9afe403f6eac1ff94379
Maroš Telka <maros@telka.sk>
2023-11-01 16:31:14 +0100

    Fix STORE instruction

--- src/instructions/mem_manipulation.rs
+++ src/instructions/mem_manipulation.rs
@@ -18,10 +18,10 @@ impl MemManipulation {
         return if let Some(captures) = regex.captures(instruction) {
             let reg1_number = if let Some(reg1) = captures.get(1) {
                 let reg1 = reg1.as_str().to_owned();
-                if reg1 == "reg0" { return Err(CannotWriteIntoReg0); }
+                if reg1 == "reg0" && load { return Err(CannotWriteIntoReg0); }
                 if reg1.is_empty() { return Err(MissingReg1); }
                 let reg1_number = reg1.trim_start_matches("reg").parse::<u8>().map_err(|_| UnsupportedReg1(reg1.clone(), 1, 5))?;
-                if reg1_number < 1 || reg1_number > 5 { return Err(UnsupportedReg1(reg1, 1, 5)); }
+                if reg1_number > 5 { return Err(UnsupportedReg1(reg1, 1, 5)); }
                 reg1_number
             } else { return Err(MissingReg1); };
 
@@ -31,7 +31,7 @@ impl MemManipulation {
                 let reg2 = reg2.as_str().to_owned();
                 if reg2.is_empty() { return Err(MissingReg2); }
                 let reg2_number = reg2.trim_start_matches("reg").parse::<u8>().map_err(|_| UnsupportedReg2(reg2.clone(), 0, 5))?;
-                if reg2_number < 0 || reg2_number > 5 { return Err(UnsupportedReg2(reg2, 0, 5)); }
+                if reg2_number > 5 { return Err(UnsupportedReg2(reg2, 0, 5)); }
                 reg2_number
             } else { return Err(MissingReg2); };
 
@@ -39,7 +39,7 @@ impl MemManipulation {
                 let imm1 = imm1.as_str().to_owned();
                 if imm1.is_empty() { return Err(MissingImm1); }
                 let imm1_number = imm1.parse::<u32>().map_err(|_| UnsupportedImm1(imm1.clone(), 65536))?;
-                if imm1_number < 0 || imm1_number > 65535 { return Err(UnsupportedImm1(imm1, 65536)); }
+                if imm1_number > 65535 { return Err(UnsupportedImm1(imm1, 65536)); }
                 imm1_number
             } else { return Err(MissingImm1); };
 
